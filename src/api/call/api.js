61350d78e96098b93c7aab12ab77eb26dcb636ad
Daniel Verdú Martínez <daniel.verdu.ext@rsi.cajarural.com>
2021-10-24 07:38:30 +0200

    configurada api call base

--- src/api/call/api.js
+++ src/api/call/api.js
@@ -2,15 +2,24 @@ import { basePath } from "../base";
 
 export default class Api{
 
-    static get(path){
+    static get(path, queryParams){
 
         const api_key = process.env.VUE_APP_PRIVATE_KEY
 
         const url = new URL(basePath)
         url.searchParams.append('api_key', api_key)
+        url.searchParams.append('language', 'es-ES')
 
         if(path){
+            
             url.pathname = url.pathname + path
+        }
+
+        if(queryParams){
+
+            for(let key in queryParams){
+                url.searchParams.append(key, queryParams[key])
+            }
         }
 
         return fetch(url)
