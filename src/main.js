886468d8a3a5f52ed9cfb7f0293b9c2c1427bbb5
Daniel Verdú Martínez <daniel.verdu.ext@rsi.cajarural.com>
2021-10-30 07:49:07 +0200

    componente section-title

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//Components
 import WebGrid from "@/components/atoms/web-grid"
+import WebSectionTitle from "@/components/atoms/web-section-title"
 
 Vue.component("web-grid", WebGrid);
+Vue.component("web-section-title", WebSectionTitle);
 
 Vue.config.productionTip = false;
 
