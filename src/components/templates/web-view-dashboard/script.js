3e50f745b2ff3bc13802689d32c216ffad42268a
Daniel Verdú Martínez <daniel.verdu.ext@rsi.cajarural.com>
2021-11-02 22:51:48 +0100

    comenzado menú

--- src/components/templates/web-view-dashboard/script.js
+++ src/components/templates/web-view-dashboard/script.js
@@ -1,3 +1,38 @@
+import WebNavbar from "@/components/organism/web-navbar";
 export default {
   name: "web-view-dashboard",
+  components: {
+    WebNavbar
+  },
+  data(){
+    return{
+      navItems: [
+        {
+          icon: "search",
+          action: "/search",
+          label: "Buscar"
+        },
+        {
+          icon: "home",
+          action: "/",
+          label: "Inicio"
+        },
+        {
+          icon: "inventory",
+          action: "/my-list",
+          label: "Mi Lista"
+        },
+        {
+          icon: "movie_creation",
+          action: "/movies",
+          label: "Películas"
+        },
+        {
+          icon: "ondemand_video",
+          action: "/tv-shows",
+          label: "Series"
+        }
+      ]
+    }
+  }
 };
